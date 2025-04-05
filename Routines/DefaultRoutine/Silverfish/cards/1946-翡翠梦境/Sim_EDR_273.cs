bb1061436dbe53c99e99896dfb0041b2e76c3632
ztzz <ztzz.xyz@gmail.com>
2025-04-05 08:24:18 +0800

    32.0.3 Card Change

--- Routines/DefaultRoutine/Silverfish/cards/1946-翡翠梦境/Sim_EDR_273.cs
+++ Routines/DefaultRoutine/Silverfish/cards/1946-翡翠梦境/Sim_EDR_273.cs
@@ -7,8 +7,8 @@ namespace HREngine.Bots
 	//法术 德鲁伊 费用：1
 	//Symbiosis
 	//共生术
-	//[x]<b>Discover</b> a <b>Choose One</b>card from another class.
-	//<b>发现</b>一张另一职业的<b>抉择</b>牌。
+	//[x]<b>Discover</b> a <b>Choose One</b>card from another class.It costs (1) less.
+	//<b>发现</b>一张另一职业的<b>抉择</b>牌，其法力值消耗减少（1）点。
 	class Sim_EDR_273 : SimTemplate
 	{
 		
